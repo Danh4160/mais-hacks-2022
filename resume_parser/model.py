e77ba92a57a8ba9c3d92e8f350aa0d38b3d4f6d7
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2022-10-02 01:36:13 -0400

    Update model.py

--- resume_parser/model.py
+++ resume_parser/model.py
@@ -1,13 +1,4 @@
-# %% 
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-import warnings
-warnings.filterwarnings('ignore')
-
-np.random.seed(1234)
-
 # class KNN:
 #     def __init__(self, X, y, k=1):
 #         self.model = KNeighborsClassifier(n_neighbors=k)
@@ -23,25 +14,18 @@ np.random.seed(1234)
 #     def split_train_test(self, X, y, test_size):
 #         return train_test_split(X, y, test_size=test_size, random_state=1, shuffle=True)
 
-    
 class modifiedknn:
-    def __init__(self, most_common_words_per_category, job_title):
-        self.common_words_dataset = most_common_words_per_category
-        self.target_category = job_title
+    def __init__(self, resume_dataset, processed_pdf_text, job_titles):
+        self.dataset = resume_dataset
+        self.X = processed_pdf_text
+        self.y = job_titles
+        self.correlations = {}  # Dictionary
 
-    def get_correlation(self, common_words_resume):
-        correlation = 0.0
+    def calculate_correlations(self):
+        
+        
 
-        return correlation
-
-    def set_relative
-
-
-
-
-
+        # Compare the the common_words_dataset from the knn with the common_words_resume
+        return self.correlations
 
     
-
-# data features are the frequency of each
-# %%
